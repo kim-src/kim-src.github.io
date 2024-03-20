8638063cc348fd6f90eb074f3d2ae47a9a124437
Chang-Seong Kim <ceobaek153@gmail.com>
2024-03-20 13:50:24 +0900

    2024-03-20 13:49 Updated data-mode using theme-toggle.js

--- _javascript/theme-toggle.js
+++ _javascript/theme-toggle.js
@@ -1,30 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
     const themeToggle = document.getElementById('theme-toggle');
-    const currentTheme = localStorage.getItem('theme');
     const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
-
+    
     // 저장된 테마가 없으면 시스템 환경에 따른 테마 적용
-    if (!currentTheme) {
-        if(prefersDarkScheme.matches) {
-            document.documentElement.setAttribute('data-theme', 'dark');
-            themeToggle.innerHTML = '<i class="fas fa-moon"></i>';
-        } else {
-            document.documentElement.setAttribute('data-theme', 'light');
-            themeToggle.innerHTML = '<i class="fa-regular fa-sun"></i>';
-        }
-    // 저장된 테마가 있으면 해당 테마 적용
-    } else {
-        document.documentElement.setAttribute('data-theme', currentTheme);
-        themeToggle.innerHTML = currentTheme === 'dark' ? '<i class="fas fa-moon"></i>' : '<i class="fa-regular fa-sun"></i>';
-    }
-
+    let theme = localStorage.getItem('theme') || (prefersDarkScheme.matches ? 'dark' : 'light');
+    
+    // 페이지 로드 시 현재 테마에 맞게 data-mode 속성 설정
+    document.documentElement.setAttribute('data-mode', theme);
+    // 토글 버튼 아이콘을 현재 테마에 맞게 설정
+    themeToggle.innerHTML = theme === 'dark' ? '<i class="fas fa-moon"></i>' : '<i class="far fa-sun"></i>';
+    
+    // 토글 아이콘에 클릭 이벤트 리스너 추가
     themeToggle.addEventListener('click', () => {
-        let theme = document.documentElement.getAttribute('data-theme');
-        let switchToTheme = theme === 'dark' ? 'light' : 'dark';
-
-        document.documentElement.setAttribute('data-theme', switchToTheme);
+        // 현재 적용된 테마를 기반으로 전환할 테마 결정
+        let currentTheme = document.documentElement.getAttribute('data-mode');
+        let switchToTheme = currentTheme === 'dark' ? 'light' : 'dark';
+        
+        // data-mode 속성을 업데이트하여 테마 전환
+        document.documentElement.setAttribute('data-mode', switchToTheme);
+        // localStorage에 새 테마 저장
         localStorage.setItem('theme', switchToTheme);
-
+        
+        // 토글 아이콘 업데이트
         themeToggle.innerHTML = switchToTheme === 'dark' ? '<i class="fas fa-moon"></i>' : '<i class="fa-regular fa-sun"></i>';
     })
 })
